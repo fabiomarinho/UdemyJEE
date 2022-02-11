5bbbdc1eca0a8a1898ac813d0ce378a2ffb9b71a
Fábio Marinho <fabiohsm@trt3.jus.br>
2022-02-11 13:48:03 -0300

    Implementing the training

---
+++ src/main/java/academy/learnprogramming/rest/TodoConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package academy.learnprogramming.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author fabio
+ */
+@ApplicationPath("api/v1")
+public class TodoConfig extends Application{
+    
+}
